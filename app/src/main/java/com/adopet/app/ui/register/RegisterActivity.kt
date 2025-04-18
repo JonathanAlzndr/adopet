c7a4a3dbea99a91b919b03378e9d9e629c95690b
Jonathan K <jkamagi41@gmail.com>
2025-04-18 22:09:12 +0800

    add layout for register

---
+++ app/src/main/java/com/adopet/app/ui/register/RegisterActivity.kt
@@ -0,0 +1,26 @@
+package com.adopet.app.ui.register
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.adopet.app.R
+import com.adopet.app.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+    }
+}
